88395f82522d1b7416aaa1e06afb83462c9754c2
Andrew Lima <andrewmll94@gmail.com>
2023-08-16 18:35:00 -0300

    Feat: Criaçao da rota GET /fertilizers.

--- src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controllers/FertilizerController.java
@@ -2,9 +2,11 @@ package com.betrybe.agrix.controllers;
 
 import com.betrybe.agrix.models.entities.Fertilizer;
 import com.betrybe.agrix.services.FertilizerService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,15 @@ public class FertilizerController {
     Fertilizer newFertilizer = fertilizerService.createFertilizers(fertilizer);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(newFertilizer);
+  }
+
+  /**
+  * Application get method to get all fertilizers.
+  */
+  @GetMapping("/fertilizers")
+  public ResponseEntity<List<Fertilizer>> getAllFertilizer() {
+    List<Fertilizer> allFertilizer = fertilizerService.getAllFertilizer();
+
+    return ResponseEntity.ok().body(allFertilizer);
   }
 }
